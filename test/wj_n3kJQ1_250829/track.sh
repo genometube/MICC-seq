b262daff89a98760584f0aded2ce73059ae0d3cb
yemingx <yemingxie@gmail.com>
2025-09-05 11:50:43 +0800

    update

---
+++ test/wj_n3kJQ1_250829/track.sh
@@ -0,0 +1,5 @@
+hicPlotTADs="/mnt/software/anaconda3/envs/hicexplorer/bin/hicPlotTADs"
+hicPlotTADs="/research/xieyeming1/software/Miniconda/envs/hicexplorer/bin/hicPlotTADs"
+${hicPlotTADs} --tracks tracks.ini.n3k_250904 --region chr7:149000000-151000000  -o micc_SE_n3k_1.png
+${hicPlotTADs} --tracks tracks.ini.n3k_250904 --region chr7:145000000-155000000  -o micc_SE_n3k_2.png
+
